ac0f86bfb48988cb1795466afa82d7f718c0dbd6
pashtet <bazhenovpavel@gmail.com>
2016-05-30 18:06:53 +0300

    impr

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # frontend-build
 
-My own build with browsersync plugin
+My own build with browsersync plugin.
+Also if you never used browsersync you need to try it!
 
 ## gulp features for styles, images, fonts
 - sourcemaps
+- globbing
 - autoprefixer for css
 - styles minifying
 - images minifying
+- browserify
+- nunjucks
+
+## scss files globing
+Previously I always forget add appropriate string to "app.scss" for new "partial.scss".
+This problem I've solved with *gulp-css-globbing* plugin which allows just include whole folder without adding all files
+
+## images minifying
+Every img placed in assets folder will minifying by appropriate gulp task using *gulp-imagemin*
+Also for better performance I use *gulp-cache*
+
+## templating with nunjucks
+*nunjuck* used for implementing templates ability
 
 ## bower
+Just few unnecessary things:
 - reset styles
 - bourbon
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 // Includes
 var gulp            = require('gulp'),
     sass            = require('gulp-sass'),
-    source          = require('vinyl-source-stream'),
-    gutil           = require('gulp-util'),
-    browserify      = require('browserify'),
+    // source          = require('vinyl-source-stream'),
+    // gutil           = require('gulp-util'),
+    // browserify      = require('browserify'),
     browserSync     = require('browser-sync').create(),
     htmlInjector    = require("bs-html-injector"),
     cssnano         = require('gulp-cssnano'),
     sourcemaps      = require('gulp-sourcemaps'),
     autoprefixer    = require('gulp-autoprefixer'),
-    concat          = require('gulp-concat'),
     uglify          = require('gulp-uglify'),
     rename          = require('gulp-rename'),
     imagemin        = require('gulp-imagemin'),
     cache           = require('gulp-cache'),
-    // reload          = browserSync.reload,
     globbing        = require('gulp-css-globbing'), //globing scss files from folders
     notify 			    = require("gulp-notify"),
     size 			      = require('gulp-size'),
@@ -79,8 +77,6 @@ gulp.task('watch', ['nunjucks','browserSync','styles'], function() {
   gulp.watch('assets/scss/**/*.scss', ['styles']);
   gulp.watch('assets/imgs/**/*.+(png|jpg|gif|svg)', ['images']);
   gulp.watch("assets/**/*.html", ['nunjucks']);
-  // gulp.watch("public/*.html", htmlInjector);
-  // gulp.watch('assets/js/*.js', ['scripts']);
 });
 
 // Default Task
